71ffd9bbd2912996d6f121fbf951e08134d00f05
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-03-06 01:58:52 +0000

    Temporarily disable authorizations

--- Controllers/AuthorizationControllers/RolesController.cs
+++ Controllers/AuthorizationControllers/RolesController.cs
@@ -12,7 +12,7 @@ namespace CloudKitchen.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "Admin")]
+    // [Authorize(Roles = "Admin")]
     public class RolesController : ControllerBase
     {
         private readonly RoleManager<IdentityRole> _roleManager;
